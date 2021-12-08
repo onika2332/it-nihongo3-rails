72bb9cf17eb7f20b9dd3b7777498e9f930e9ab41
my name <me@example.com>
2021-12-08 18:43:43 +0000

    Add age & name to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_182325) do
+ActiveRecord::Schema.define(version: 2021_12_08_184043) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
